c96928d3fb19930d5a67f57bdde85c8194f62bf3
mitchnm <mitchndungu21@gmail.com>
2019-05-12 16:32:06 +0300

    updated credentials class

---
+++ credentials.py
@@ -0,0 +1,13 @@
+class Credentials:
+    '''
+    class that generates new credentials intsances
+    '''
+    
+    credentials_list = [] 
+
+    def __init__(self,account_name,password):
+        '''
+        init method to create instances of the credentials class
+        '''
+        self.account_name = account_name
+        self.password = password
