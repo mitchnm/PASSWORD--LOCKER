e5d4cb9a9a7a943a8cee3c1a2b3dbd6810752113
mitchnm <mitchndungu21@gmail.com>
2019-05-12 14:50:58 +0300

    added starting options

--- start.py
+++ start.py
@@ -1,14 +1,15 @@
 def mitch():
   print("WELCOME TO PASSWORD-LOCKER!!")
+def mitch1():  
   print("Would you like to continue? (yes/no)")
   answer = input() 
-  if answer is "yes":
+  if answer == 'yes':
     print("Okay.Press 1 to continue.")
-  elif answer is "no":
-    print("Thanks for using our application.")
+  elif answer == 'no':
+    print("Thanks for using our application.we hope to see you again.")
   else:
     print("Invalid choice.Try again.")  
-    mitch()
+    mitch1()
 
 
 
@@ -18,3 +19,4 @@ def mitch():
 
 
 mitch()
+mitch1()
