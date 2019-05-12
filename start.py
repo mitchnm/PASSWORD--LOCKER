d98258b6622d11449a0f1846041294bd761d2432
mitchnm <mitchndungu21@gmail.com>
2019-05-12 14:56:26 +0300

    added starting options2

--- start.py
+++ start.py
@@ -3,9 +3,17 @@ def mitch():
 def mitch1():  
   print("Would you like to continue? (yes/no)")
   answer = input() 
-  if answer == 'yes':
+  if answer == "yes":
     print("Okay.Press 1 to continue.")
-  elif answer == 'no':
+    one = input()
+    if one == 1:
+      print("Welcome to password-locker.Would you like to (sign up/log in")
+      log_sign = input()
+      if log_sign == "sign up":
+        print("Awesome.Enter your preffered username.")
+    else:
+      print("Are you that stupid.Please press 1")  
+  elif answer == "no":
     print("Thanks for using our application.we hope to see you again.")
   else:
     print("Invalid choice.Try again.")  
